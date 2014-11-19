866640f5efab0706636887cc275958aa045a7fc9
Vaibhav Somani <njoy.today@gmail.com>
2014-11-19 11:42:45 -0800

    added send to dumb functionality

--- areyoudumb.py
+++ areyoudumb.py
@@ -5,8 +5,15 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def hello_dumb():
     return render_template('dumb.html')
+
+
+@app.route('/<dumb_id>')
+def send_dumb(dumb_id):
+    print('here')
+    return render_template('youdumb.html')
+
 
 if __name__ == '__main__':
     app.run()
--- static/js/dumb.js
+++ static/js/dumb.js
@@ -12,4 +12,26 @@ $(function(){
             time = true;
         }
     }, 3000);
+
+    $("#send_to_dumb").click(function(){
+        var dumb_link = (new Date().getTime()).toString(30);
+        window.location.href = window.location.href + dumb_link;
+    });
+
+    $("div.dumber-img").hover(function(){
+        moveDumbElement($(this));
+    }, function(){
+        moveDumbElement($(this));
+    });
 });
+
+function moveDumbElement(elem){
+    if(Math.round(Math.random()*100) %2 == 0)
+        $(elem).css("top", getRandomArbitrary(10,60)+"%");
+    else
+        $(elem).css("left", getRandomArbitrary(10, 70)+"%");
+}
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
